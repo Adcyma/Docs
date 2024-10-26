e0bceca030e62a3adcb2d95cfcba0e59146bfb5a
Fredrik Schöld <71184994+FredrikSchold@users.noreply.github.com>
2024-10-26 14:12:05 +0200

    yee

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/fredrik/Documents/GitHub/Adcyma-Docs/Docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/fredrik/Documents/GitHub/Adcyma-Docs/Docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/fredrik/Documents/GitHub/Adcyma-Docs/Docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/fredrik/Documents/GitHub/Adcyma-Docs/Docs/src/css/custom.css"),
+];
